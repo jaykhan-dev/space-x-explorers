b74a5cf13192f65c358bf173a638fb4b0632d53b
jaykhan-dev <jaykhan.sound@gmail.com>
2022-12-16 12:24:29 -0500

    added toml and netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-		adapter: adapter()
+	kit: {
+		adapter: adapter({
+			edge: false,
+			split: false
+		})
 	},
 
-    preprocess: [preprocess({
-        postcss: true
-    })]
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
